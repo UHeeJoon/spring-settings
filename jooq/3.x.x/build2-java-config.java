c0b5a72ed347db3737fb2d20838ead15f22c70d1
UHeeJoon <yhjyhj3855@gmail.com>
2025-02-12 20:16:26 +0900

    feat: jooq dsl 설정

---
+++ jooq/3.x.x/build2-java-config.java
@@ -0,0 +1,23 @@
+import javax.sql.DataSource;
+
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.jooq.impl.DataSourceConnectionProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
+
+@Configuration
+public class DslConfig {
+
+    @Bean
+    public DataSourceConnectionProvider connectionProvider(DataSource dataSource) {
+        return new DataSourceConnectionProvider(new TransactionAwareDataSourceProxy(dataSource));
+    }
+
+    @Bean
+    public DSLContext dsl(DataSource dataSource) {
+        return DSL.using(connectionProvider(dataSource), SQLDialect.MYSQL);
+    }
+}
